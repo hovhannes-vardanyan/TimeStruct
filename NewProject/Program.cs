68c744931ece16b237385d67c582ff2a009d70d4
hovovardanyan <57057585+hovovardanyan@users.noreply.github.com>
2020-01-15 16:32:48 +0400

    Добавьте файлы проекта.

---
+++ NewProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t1 = new Time(new DateTime(year: 2020, month: 1, day: 25, hour: 10, minute: 35, second: 50));
+            Time t2 = new Time(new DateTime(year: 2020, month: 1, day: 25, hour: 8, minute: 45, second: 50));
+
+            Console.WriteLine($"TimeOne {t1}");
+            Console.WriteLine($"TimeTwo {t2}");
+            Console.WriteLine("Operatiors");
+            Time t3 = t1 + t2;
+            Console.WriteLine($"TimeOne + TimeTwo {t3}");
+            Time t4 = t1 - t2;
+            Console.WriteLine($"TimeOne - TimeTwo {t4}");
+
+            Console.WriteLine("Casting");
+            //implicit  int -> Time
+            int temp = 55556546;
+            Time t5 = temp;
+            Console.WriteLine(t5);
+            // explicit Time -> int
+            int a = (int)t5;
+            Console.WriteLine(a);
+
+
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NewProject/Time.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NewProject
+{
+    struct Time
+    {
+        private int maxHour { get => 24; }
+        public int TotalTimeInMinutes { get; set; }
+        public int NumberOfMinutes { get; set; }
+        public int NumberOfHours { get; set; }
+        public int Noon { get => 720; }
+
+
+
+        public Time(int value)
+        {
+            this.TotalTimeInMinutes = value;
+            // ete jamy 24ic mec e vercvume mnacordy
+            int temp = (int)Math.Floor((double)value / 60);
+            if (temp >= 24)
+            {
+                Console.WriteLine("Hour is Bigger than 24.");
+                this.NumberOfHours = temp % 24;
+            }
+            else
+            {
+                this.NumberOfHours = (int)Math.Floor((double)value / 60);
+            }
+
+            this.NumberOfMinutes = value % 60;
+        }
+        public Time(DateTime time)
+        {
+            this.TotalTimeInMinutes = (time.Hour) * 60 + time.Minute;
+            this.NumberOfHours = time.Hour;
+            this.NumberOfMinutes = time.Minute;
+        }
+
+        // operator overload
+        public static Time operator +(Time timeOne, Time timeTwo)
+        {
+            return new Time(timeOne.TotalTimeInMinutes + timeTwo.TotalTimeInMinutes);
+        }
+        public static Time operator -(Time timeOne, Time timeTwo)
+        {
+            return new Time(timeOne.TotalTimeInMinutes - timeTwo.TotalTimeInMinutes);
+        }
+
+        //implicit explicit cast
+        static public implicit operator Time(int value)
+        {
+            return new Time(value);
+        }
+        static public explicit operator int(Time value)
+        {
+            return value.TotalTimeInMinutes;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.NumberOfHours}:{this.NumberOfMinutes}";
+        }
+
+    }
+}
